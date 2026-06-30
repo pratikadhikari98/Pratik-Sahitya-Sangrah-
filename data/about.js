2c08d3a9cbe5412d951eb55ca6fe00251dbff1fe
Pratik Adhikari <amababa055@gmail.com>
2026-06-30 18:23:09 +0545

    Update about.js

--- data/about.js
+++ data/about.js
@@ -5,10 +5,9 @@
 const ABOUT_DATA = {
   name: "प्रतीक",
   subtitle: "कवि • लेखक • विचारक",
-  location: "काठमाडौं, नेपाल",
+  location: "पोखरा, नेपाल",
   photo: "covers/about-photo.jpg",   // ← आफ्नो photo यहाँ राख्नुस्
   photoEmoji: "✍️",                  // photo नभए यो देखिन्छ
-
   contact: {
     facebook: "https://facebook.com/",    // ← आफ्नो Facebook link राख्नुस्
     youtube: "https://youtube.com/",      // ← आफ्नो YouTube link राख्नुस्
